528f88b1029ee7480363314ced6adfa76c713a08
ibrakhim2906 <ibrakhim2906@yahoo.com>
2026-08-21 19:01:32 +0500

    ...

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    { "<leader>f", function() require("conform").format({ lsp_fallback = true }) end, desc = "Format buffer" },
+  },
+  opts = {
+    formatters_by_ft = {
+      cpp = { "clang_format" },
+      c = { "clang_format" },
+      python = { "ruff_format" },
+    },
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+  },
+}
